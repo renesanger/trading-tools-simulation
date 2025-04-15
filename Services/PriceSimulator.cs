1fc753ab47a6bab67b038e3be1ab52c95f010ac0
renesanger <renejsanger@gmail.com>
2025-04-15 16:58:32 -0400

    Added Price Simulation functionality

--- Services/PriceSimulator.cs
+++ Services/PriceSimulator.cs
@@ -1,6 +1,8 @@
 public class Simulator 
 {
     private Random _random = new Random();
+    public decimal CurrentPrice { get; private set; }
+    public Timer? _timer;
 
     public Price GetPrice()
     {
@@ -9,4 +11,24 @@ public class Simulator
             LastUpdated = DateTime.UtcNow
         };
     }
+
+    public void PriceSimulator()
+    {
+        _timer = new Timer(new TimerCallback(UpdatePrice), null, TimeSpan.Zero, TimeSpan.FromSeconds(2));
+    }
+
+    private void UpdatePrice(object state)
+    {
+        CurrentPrice = 100 + (decimal)(_random.NextDouble() * 10);
+        //Random Fluctuation
+    }
+
+    public Price GetCurrentPrice()
+    {
+        return new Price(){
+            CurrentPrice = CurrentPrice,
+            LastUpdated = DateTime.UtcNow
+        };
+    }
+
 }
